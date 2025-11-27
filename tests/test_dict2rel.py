89cb2fd5f338428993773e23b90c62b3249f490c
Jacob Morris <blendingjake@gmail.com>
2025-11-26 20:55:46 -0500

    Added initial testing for pandas and polars

--- tests/test_dict2rel.py
+++ tests/test_dict2rel.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import polars as pl
+import pytest
+
 from dict2rel import dict2rel, rel2dict
 
 EXAMPLE = {
@@ -39,9 +43,41 @@ def test_basic():
     assert "_value" in tables["*.name.suffixes"][0]
 
 
-def test_to_tables_and_back():
+@pytest.mark.parametrize(
+    "provider",
+    [
+        pd.DataFrame,
+        pl.DataFrame
+    ]
+)
+def test_basic_with_providers(provider):
+    """Test basic JSON -> tables with various providers"""
+    tables = dict2rel(EXAMPLE, provider)
+
+    assert all(isinstance(v, provider) for v in tables.values())
+
+    primary = tables["*"]
+    assert "name.first" in primary
+    assert primary["name.first"].to_list() == [EXAMPLE["name"]["first"]]
+
+
+def test_to_tables_and_back_basic():
     """Test JSON -> tables -> JSON"""
     tables = dict2rel([EXAMPLE], lambda x: x)
     og = rel2dict(tables)
 
+    assert og == [EXAMPLE]
+
+
+@pytest.mark.parametrize(
+    "provider,to_rows",
+    [
+        (pd.DataFrame, lambda t: (row for _, row in t.iterrows())),
+        (pl.DataFrame, lambda t: t.rows(named=True))
+    ]
+)
+def test_to_tables_and_back_providers(provider, to_rows):
+    """Test JSON -> provider tables -> JSON"""
+    tables = dict2rel(EXAMPLE, provider)
+    og = rel2dict(tables, to_rows)
     assert og == [EXAMPLE]
